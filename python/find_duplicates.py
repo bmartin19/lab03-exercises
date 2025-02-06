e5654e691d8572d4d54e966170e6ddecccb12bc2
bmartin19 <bmartin3@unca.edu>
2025-02-05 23:08:59 -0500

    added dictionary method to find_duplicates.py

--- python/find_duplicates.py
+++ python/find_duplicates.py
@@ -7,6 +7,21 @@ def find_duplicates_nested_loop(l: list) -> list:
 
     return duplicates
 
+def find_duplicates_dict(l: list) -> list:
+
+    freq_map = {}
+    duplicates = []
+
+    for i in l:
+        if i in freq_map:
+            if freq_map[i] == 1:
+                duplicates.append(i)
+            freq_map[i] += 1
+        else:
+            freq_map[i] = 1
+
+    return duplicates
+
 if __name__ == "__main__":
     sample1 = [3, 7, 5, 6, 7, 4, 8, 5, 7, 66]
     sample2 = [3, 5, 6, 4, 4, 5, 66, 6, 7, 6]
@@ -17,3 +32,8 @@ if __name__ == "__main__":
     print("Sample 2:", find_duplicates_nested_loop(sample2))
     print("Sample 3:", find_duplicates_nested_loop(sample3))
     print("Sample 4:", find_duplicates_nested_loop(sample4))
+
+    print("Sample 1:", find_duplicates_dict(sample1))
+    print("Sample 2:", find_duplicates_dict(sample2))
+    print("Sample 3:", find_duplicates_dict(sample3))
+    print("Sample 4:", find_duplicates_dict(sample4))
